03045828658d4ecf35031d3545944c6217a07b12
SSong-SSong <gksthf3609@gmail.com>
2023-04-11 14:43:09 +0900

    new

--- krri_streamlit.py
+++ krri_streamlit.py
@@ -3,6 +3,10 @@ import streamlit as st
 import streamlit.components.v1 as com
 com.html(""" 
 <div>
-<h1><a>img class="logo" src="krri_logo.svg"</a>Linear Regression Model을 통한 철도역사 에너지사용량 예측</h1>
+  <div style='
+    background-color:#F6CECE'
+    font-color:#424242
+  '>
+  <h1>Linear Regression Model을 통한 철도역사 에너지사용량 예측</h1>
 </div>
 """)
